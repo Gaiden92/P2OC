49ddc52ae44e2231f2f6c6cf7f8c032dc72a2d17
Gaiden92 <samy_fouchal@hotmail.fr>
2023-07-31 16:17:34 +0200

    modification de la fonction getAllCategoriesTitles

--- home.py
+++ home.py
@@ -3,21 +3,21 @@ from fonctions import *
 
 # création d'un dictionnaire contenant les noms des catagories et leurs liens
 dictionnary_categories_name_and_url = getAllCategoriesTitles(URL_HOME)
-
 print(dictionnary_categories_name_and_url)
 
-# création du dossier "Catégorie" et des fichiers excels pour chaque catégorie
-try:
-    os.mkdir("Categories")
 
-except FileExistsError:
-    print("Le dossier existe déjà.")
+# # création du dossier "Catégorie" et des fichiers excels pour chaque catégorie
+# try:
+#     os.mkdir("Categories")
 
-create_excel_files_by_categories_names("Categories", dictionnary_categories_name_and_url)
+# except FileExistsError:
+#     print("Le dossier existe déjà.")
 
-# création du dictionnaire contenant les urls des livres par catégories
-dictionnary_all_books_urls_by_category = get_links_articles_by_categories(dictionnary_categories_name_and_url)
+# create_excel_files_by_categories_names("Categories", dictionnary_categories_name_and_url)
+
+# # création du dictionnaire contenant les urls des livres par catégories
+# dictionnary_all_books_urls_by_category = get_links_articles_by_categories(dictionnary_categories_name_and_url)
 
 
-# impression des données dans les fichiers excels et téléchargements des images
-save_data_by_categorie(dictionnary_all_books_urls_by_category)
+# # impression des données dans les fichiers excels et téléchargements des images
+# save_data_by_categorie(dictionnary_all_books_urls_by_category)
