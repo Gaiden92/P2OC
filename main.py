dbd32757df34e2d06ab1e6d8b7b93cab45482853
Gaiden92 <samy_fouchal@hotmail.fr>
2023-08-01 18:08:05 +0200

    changement nom du fichier "home" en "main

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from fonctions import *
 # création d'un dictionnaire contenant les noms des catagories et leurs liens
 dictionnary_categories_name_and_url = getAllCategoriesTitles(URL_HOME)
 
-
-
 # création du dossier "Catégorie" et des fichiers excels pour chaque catégorie
 try:
     os.mkdir("Categories")
@@ -17,7 +15,6 @@ create_excel_files_by_categories_names("Categories", dictionnary_categories_name
 
 # création du dictionnaire contenant les urls des livres par catégories
 dictionnary_all_books_urls_by_category = get_links_articles_by_categories(dictionnary_categories_name_and_url)
-
 
 # impression des données dans les fichiers excels et téléchargements des images
 save_data_by_categorie(dictionnary_all_books_urls_by_category)
