7d9ca57f93defb8ea3e94ea53e5465506229b0c7
Gaiden92 <samy_fouchal@hotmail.fr>
2023-08-07 14:56:15 +0200

    modification commentaires

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ start_time = int(time.time())
 # création d'un dictionnaire contenant les noms des catégories et leurs liens
 dictionnary_categories_name_and_url = get_all_categories_titles(URL_HOME)
 
-# création d'un dictionnaire contenant les urls des livres par catégories
+# création d'un dictionnaire contenant les liens de chaque livre par catégories
 dictionnary_all_books_urls_by_category = get_links_books_by_categories(dictionnary_categories_name_and_url)
 
 # sauvegarde des données
